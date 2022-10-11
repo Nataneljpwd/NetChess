7dcef7516b7a263e8e8f3a872ed8a89951f9fa1d
Natanel Rudyuklakir <natanelrudyuklakir@gmail.com>
2022-10-11 18:16:09 +0300

    finished draw check start of Net-client

--- Player.java
+++ Player.java
@@ -10,21 +10,48 @@ public class Player {
     public boolean isWhite;
     public boolean ourTurn;
     private List<Piece> pieces;
+    private ConnectionHandler ch;
+    String host="localhost";
+    int port = 8888;
 
-    public Player(boolean isWhite){
+    public Player(){
         this.pieces=new ArrayList<>();
-        this.isWhite = isWhite;
-        this.ourTurn=this.isWhite;
-        this.board=new Board(isWhite,pieces);
+        this.ch=new ConnectionHandler(host,port);
+        this.ch.run();
     }
 
+    public ConnectionHandler getConnectionHandler(){
+        return this.ch;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Player p=new Player();
+        p.playerSetup(p.getConnectionHandler().getIsWhite());
+    }
+
+    public void playerSetup(boolean isWhite){
+        this.isWhite=isWhite;
+        this.ourTurn=this.isWhite;
+        this.board=new Board(this.isWhite, this.pieces);
+    }
+
+    //add a check for what pieces there are to know if its draw!.
     public boolean checkDraw(){
+        int c=0;
         for(Piece p : pieces){
+            if(p instanceof Bishop || p instanceof Knight){c++;}
+            
             if(p.possibleMoves.size()>0){
+                if(!(p instanceof King )&& !(p instanceof Bishop) && !(p instanceof Knight))c-=100;
                 return false;
             }
         }
-        return true;
+        //c is equal to 1 if we have only a bishop or only a knight
+        if(c==1){
+            // only the bishop or the knight is left
+            return true;
+        }
+        return true ;
     }
 
     public void calculateMoves(){
@@ -67,9 +94,84 @@ public class Player {
         }
         return 0;
     }
+
+    public void move(){
+        //TODO think of implementation
+    }
     
     //will add the listener in the gameview in android studio which willl be a thread.
-    //create inner class for connection handling------------------------------
-    //will calculate moves for all Pieces before every move and check for draw mate and check.
+    //it will call the move function
 
+
+    private class ConnectionHandler implements Runnable{
+
+        private Socket s;
+        private BufferedReader reader;
+        private PrintWriter writer;
+        private boolean ourTurn;
+        private boolean isWhite;
+        private boolean isGameOver=false;
+        private String move;
+
+        public ConnectionHandler(String host,int port){
+            try {
+                s=new Socket(host,port);
+                reader=new BufferedReader(new InputStreamReader(s.getInputStream()));
+                writer=new PrintWriter(s.getOutputStream(),true);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public boolean getIsWhite(){
+            return isWhite;
+        }
+
+        public void setMove(String mv){
+            this.move=mv;
+        }
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            String msg="";
+            try {
+                //first we send a message wheter the playee is white of=r black
+                msg=this.reader.readLine();
+                this.isWhite = Integer.parseInt(msg)%2==0;
+                this.ourTurn=this.isWhite;    
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+            while(!this.isGameOver){
+                try{
+                    if(ourTurn){
+                        if(!this.move.equals("")){
+                            
+                            this.move="";
+                            ourTurn=!ourTurn;
+                        }
+                    }else{
+
+                        ourTurn=!ourTurn;
+                    }
+                }catch(Exception e){
+                    //this.isGameOver=true;
+                    //TODO handle exception
+                }finally{
+                    try {
+                        this.isGameOver=true;
+                        this.reader.close();
+                        this.writer.close();
+                        this.s.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }               
+            }          
+        }
+
+    }
 }
